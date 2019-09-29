f69238bb28849aa27316bb774f3933b723e54609
Summi1986 <kumar.sumit818@gmail.com>
2019-09-29 15:44:02 +0530

    Recipe app intial set up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [ new Recipe('Test Recipe', 'This is simply a Recipe', '')];
+  recipes: Recipe[] = [ 
+    new Recipe('Speedy Tomato Pasta', 'This is simply a Recipe', 'https://deliciouslyella.com/wp-content/uploads/2019/07/speedy-tomato-pasta-recipe-2.jpg'),
+    new Recipe('Super Burger Mania', 'This is simply a Recipe', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/exps28800_UG143377D12_18_1b_RMS-696x696.jpg')];
 
   constructor() { }
 
